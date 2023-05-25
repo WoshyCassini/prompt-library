7c423b4a8e061535ab28bfb20843cec502ad7c99
Daniel Wochnik <daniel.wochnik@cassini.de>
2023-05-25 15:27:32 +0200

    Infinite scrolling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Tile from './Tile';
 import './Home.css'; // Import the CSS file for styling
 
 const Home = () => {
   const [visibleTiles, setVisibleTiles] = useState(10); // Number of initially visible tiles
+  const [isLoading, setIsLoading] = useState(false); // Loading state for fetching more tiles
+  const tileRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const target = entries[0];
+        if (target.isIntersecting && !isLoading) {
+          // When the last tile is visible and not already loading, load more tiles
+          setIsLoading(true);
+        }
+      },
+      { threshold: 1 }
+    );
+
+    if (tileRef.current) {
+      observer.observe(tileRef.current);
+    }
+
+    return () => {
+      if (tileRef.current) {
+        observer.unobserve(tileRef.current);
+      }
+    };
+  }, [isLoading]);
+
+  useEffect(() => {
+    if (isLoading) {
+      // Simulate API call or data fetch delay
+      setTimeout(() => {
+        setVisibleTiles((prevVisibleTiles) => {
+          const nextVisibleTiles = prevVisibleTiles + 10;
+          return Math.min(nextVisibleTiles, data.length);
+        });
+        setIsLoading(false);
+      }, 1000); // Delay for demonstration purposes, replace with actual API call
+    }
+  }, [isLoading]);
 
   // Mocked data entries
   const data = [
@@ -34,22 +72,15 @@ const Home = () => {
 
   const visibleData = data.slice(0, visibleTiles);
 
-  const loadMoreTiles = () => {
-    const nextVisibleTiles = visibleTiles + 10;
-    setVisibleTiles(Math.min(nextVisibleTiles, data.length));
-  };
-
   return (
     <div className="home-container">
       {visibleData.map((item, index) => (
         <Tile key={index} title={item.title} description={item.description} />
       ))}
 
-      {visibleTiles < data.length && (
-        <button className="load-more-button" onClick={loadMoreTiles}>
-          Load More
-        </button>
-      )}
+      {isLoading && <div className="loading-message">Loading...</div>}
+
+      <div ref={tileRef} />
     </div>
   );
 };
